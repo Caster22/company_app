9a64857d94e24ddaa40f2870ffcb33ae97fca2ad
Michał Kopacz <kopaczmichal093@gmail.com>
2020-09-18 22:59:04 +0200

    Add use of db for departments.routes.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
 const mongoClient = require('mongodb').MongoClient;
+
+const employeesRoutes = require('./routes/employees.routes');
+const departmentsRoutes = require('./routes/departments.routes');
+const productsRoutes = require('./routes/products.routes');
 
 mongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
   if (err){
@@ -9,21 +12,18 @@ mongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUni
   }
   else {
     console.log('Successfully connected to the database');
+
     const db = client.db('companyDB');
-
-    db.collection('employees').find({ department: 'IT' }).toArray((err, data) => {
-      if(!err) {
-        console.log(data)
-      }
-    });
-
-    const employeesRoutes = require('./routes/employees.routes');
-    const departmentsRoutes = require('./routes/departments.routes');
-    const productsRoutes = require('./routes/products.routes');
+    const app = express();
 
     app.use(cors());
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
+
+    app.use((req, res, next) => {
+      req.db = db;
+      next();
+    });
 
     app.use('/api', employeesRoutes);
     app.use('/api', departmentsRoutes);
