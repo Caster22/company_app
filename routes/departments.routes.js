016c28f0241137ff3bd1ad0141f652ee25e0e219
Michał Kopacz <kopaczmichal093@gmail.com>
2020-09-20 14:00:49 +0200

    Change return log on put and delete in departments.routes.js

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -55,7 +55,7 @@ router.put('/departments/:id', async (req, res) => {
     const dep = await(Department.findById(req.params.id));
     if (dep) {
       await Department.updateOne({ _id: req.params.id }, { $set: { name: name } });
-      res.json({ message: 'OK' });
+      res.json({ message: dep });
     } else {
       res.status(404).json({ message: 'Not found' });
     }
@@ -70,7 +70,7 @@ router.delete('/departments/:id', async (req, res) => {
     const dep = await(Department.findById(req.params.id));
     if (dep) {
       await Department.deleteOne({ _id: req.params.id});
-      res.json({ message: 'OK' });
+      res.json({ message: dep });
     } else {
       res.status(404).json({ message: 'Not found' });
     }
