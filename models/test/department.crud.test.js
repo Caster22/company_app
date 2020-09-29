96de93977eeee54eb9d2c3a9657f962ebdba4e13
Michał Kopacz <kopaczmichal093@gmail.com>
2020-09-29 16:27:09 +0200

    Complete tests for Department.crud

--- models/test/department.crud.test.js
+++ models/test/department.crud.test.js
@@ -6,22 +6,16 @@ const MongoMemoryServer = require('mongodb-memory-server').MongoMemoryServer;
 
 describe('Department', () => {
 	before(async () => {
-
 		try {
 			const fakeDB = new MongoMemoryServer();
-
 			const uri = await fakeDB.getUri();
-
 			mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
 		} catch(err) {
 			console.log(err);
 		}
-
 	});
 
 	describe('Reading data', () => {
-
 		before(async () => {
 			const testDepOne = new Department({ name: 'Department #1' });
 			await testDepOne.save();
@@ -45,6 +39,88 @@ describe('Department', () => {
 
 		after(async () => {
 			await Department.deleteMany();
+		});
+	});
+
+	describe('Creating data', () => {
+		it('should should insert new document with "insertOne" method', async function () {
+			const department = new Department({ name: 'Department #1'});
+			await department.save();
+			expect(department.isNew).to.be.false;
+		});
+
+		after(async () => {
+			await Department.deleteMany();
+		});
+	});
+
+	describe('Updating data', () => {
+		beforeEach(async () => {
+			const testDepOne = new Department({ name: 'Department #1' });
+			await testDepOne.save();
+
+			const testDepTwo = new Department({ name: 'Department #2' });
+			await testDepTwo.save();
+		});
+
+		afterEach(async () => {
+			await Department.deleteMany();
+		});
+
+		it('should properly update one document with "updateOne" method' ,async function () {
+			await Department.updateOne({ name: 'Department #1' }, { $set: { name: '=Department #1=' }});
+			const updatedDepartment = await Department.findOne({ name: '=Department #1=' });
+			expect(updatedDepartment).to.not.be.null;
+		});
+
+		it('should properly update one document with "save" method',async function () {
+			const department = await Department.findOne({ name: 'Department #1' });
+			department.name = '=Department #1=';
+			await department.save();
+			//Or:
+			//const updatedDepartment = await Department.findOne({ name: '=Department #1=' });
+			//expect(updatedDepartment).to.not.be.null;
+			expect(department.isNew).to.be.false;
+		});
+
+		it('should properly update multiple documents with "updateMany" method',async function () {
+			await Department.updateMany({}, {$set: { name: 'UpdateMany' }});
+			const updDepartments = await Department.find();
+			expect(updDepartments[0].name).to.be.equal('UpdateMany');
+			expect(updDepartments[1].name).to.be.equal('UpdateMany');
+		});
+	});
+
+	describe('Removing data', () => {
+		beforeEach(async () => {
+			const testDepOne = new Department({ name: 'Department #1' });
+			await testDepOne.save();
+
+			const testDepTwo = new Department({ name: 'Department #2' });
+			await testDepTwo.save();
+		});
+
+		afterEach(async () => {
+			await Department.deleteMany();
+		});
+
+		it('should  properly remove one document with "deleteOne" method',async function () {
+			await Department.deleteOne({ name: 'Department #1' });
+			const deletedDepartment = await  Department.findOne({ name: 'Department #1' });
+			expect(deletedDepartment).to.be.null;
+		});
+
+		it('should  properly remove one document with "remove" method',async function () {
+			const department = await Department.findOne({ name: 'Department #1' });
+			await department.remove();
+			const removedDepartment = await Department.findOne({ name: 'Department #1' });
+			expect(removedDepartment).to.be.null;
+		});
+
+		it('should  properly remove multiple documents with "deleteMany" method',async function () {
+			await Department.deleteMany();
+			const deletedDeparts  = await Department.find();
+			expect(deletedDeparts.length).to.be.equal(0);
 		});
 	});
 });
